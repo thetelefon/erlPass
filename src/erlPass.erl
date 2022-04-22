90ae6cab8a7297607ceeeaa33ee8e6bcfca4274f
theTelefon <robert.lasu@gmail.com>
2022-04-22 17:11:29 +0200

    Syntax-update in function generate and documentation

--- src/erlPass.erl
+++ src/erlPass.erl
@@ -1,5 +1,10 @@
--module(erlPass).
+%% @doc A password generator.
+%% This is the following text
+%% @author Robert Lasu
+%% @version 0.1.0
+%% @copyright 2022 Robert Lasu
 
+-module(erlPass).
 -export([generate/5]).
 
 gen_number(B) ->
@@ -11,7 +16,7 @@ gen_upper(B) ->
 gen_lower(B) ->
     [97 + (B rem 26)].
 
-gen_symbol(B, A) ->
+gen_symbol({B, A}) ->
     case B rem 4 of
         0 ->
             [32 + A rem 16];
@@ -27,40 +32,53 @@ seed() ->
     <<A:24, B:24>> = crypto:strong_rand_bytes(6),
     {A,B}.
 
-generate(Length,_,_,_,_) when Length < 1 -> {error, invalid_length};
-generate(Length, Upper, Lower, Number, Symbols) -> generate(Length, Upper, Lower, Number, Symbols, []).
 
-generate(0,_,_,_,_, List) -> lists:flatten(List);
-generate(Length, Upper, Lower, Number, Symbols, List) ->
+%% @doc Generates a password.
+%% @end
+-spec generate(Length, Upper, Lower, Number, Symbol) -> Pass when
+    Length  ::integer(),
+    Upper   ::boolean(),
+    Lower   ::boolean(),
+    Number  ::boolean(),
+    Symbol  ::boolean(),
+    Pass    ::list().
+generate(Length,_,_,_,_) when Length < 1 -> {error, invalid_length};
+generate(Length, Upper, Lower, Number, Symbol) -> generate(Length, Upper, Lower, Number, Symbol, []).
+
+%% @doc Generate a password
+%% see {@link generate/5}
+%% @end
+-spec generate(Length, Upper, Lower, Number, Symbol, Pass) -> RetPass when
+    Length  ::integer(),
+    Upper   ::boolean(),
+    Lower   ::boolean(),
+    Number  ::boolean(),
+    Symbol  ::boolean(),
+    Pass    ::list(),
+    RetPass ::list().
+generate(_,false,false,false,false,_) -> {error, no_char};
+generate(0,_,_,_,_, Pass) -> lists:flatten(Pass);
+generate(Length, Upper, Lower, Number, Symbol, Pass) ->
     {A, B} = seed(),
 
     case A rem 4 of
-        0 ->
-            if
-            Number ->
-                generate(Length-1, Upper, Lower, Number, Symbols, [gen_number(B) | List]);
-            true ->
-                generate(Length, Upper, Lower, Number, Symbols, List)
-            end;
-        1 ->
-            if
-            Upper ->
-                generate(Length-1, Upper, Lower, Number, Symbols, [gen_upper(B) | List]);
-            true ->
-                generate(Length, Upper, Lower, Number, Symbols, List)
-            end;
-        2 ->
-            if
-            Lower ->
-                generate(Length-1, Upper, Lower, Number, Symbols, [gen_lower(B) | List]);
-            true ->
-                generate(Length, Upper, Lower, Number, Symbols, List)
-            end;
-        3 ->
-            if
-            Symbols ->
-                generate(Length-1, Upper, Lower, Number, Symbols, [gen_symbol(B, A) | List]);
-            true ->
-                generate(Length, Upper, Lower, Number, Symbols, List)
-            end
+        0 -> generate(Number, Length, Upper, Lower, Number, Symbol, fun gen_number/1, B, Pass);
+        1 -> generate(Upper, Length, Upper, Lower, Number, Symbol, fun gen_upper/1, B, Pass);
+        2 -> generate(Lower, Length, Upper, Lower, Number, Symbol, fun gen_lower/1, B, Pass);
+        3 -> generate(Symbol, Length, Upper, Lower, Number, Symbol, fun gen_symbol/1, {B,A}, Pass)
         end.
+
+-spec generate(ToGen, Length, Upper, Lower, Number, Symbol, Fun, Args, Pass) -> RetPass when
+    ToGen       :: boolean(),
+    Length      :: integer(),
+    Upper       :: boolean(),
+    Lower       :: boolean(),
+    Number      :: boolean(),
+    Symbol      :: boolean(),
+    Fun         :: function(),
+    Args        :: term(),
+    Pass        :: list(),
+    RetPass     :: list().
+generate(_, 0, _,_,_,_,_,_, Pass) -> Pass;
+generate(true, Length, U, L, N, S, Fun, B, Pass) -> generate(Length-1, U, L, N, S, [ Fun(B) | Pass]);
+generate(false, Length, U, L, N, S, _, _, Pass) -> generate(Length, U, L, N, S, Pass).
